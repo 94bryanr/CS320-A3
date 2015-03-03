255b94ee36154b641d9e4d1f0593ca275387d301
Bryan Reilly <94bryanr@gmail.com>
2015-03-03 01:20:35 -0700

    Working q1, restarted q2

--- q2.py
+++ q2.py
@@ -1,21 +1,7 @@
-import numpy
-
-def transitiveClosure(array):
-	#Initialize returned array
-	closure = array
-	#iterate through each node n
-	for n in range(array.shape[1]):
-		#dfs from n to find all reachable nodes from n
-		reachable = DFS(n, array)
-		#add all reachable nodes to n's edges
-		for i in reachable:
-			closure[n][i] = 1
-	return closure
-
 def DFS(node, array):
 	#Initialize return list
 	final = []
-	#Remeber initial node
+	#Starter node
 	S = [node]
 	#Initialize E to be a list representing the explored state of each node
 	E = [0] * array.shape[1]
@@ -38,19 +24,3 @@ def DFS(node, array):
 	#remove reference to self:
 	if node in final: final.remove(node)
 	return final
-
-a = numpy.zeros([6,6])
-#[from][to]
-#[y][x]
-a[0][1] = 1
-a[0][3] = 1
-a[0][4] = 1
-a[1][2] = 1
-a[3][5] = 1
-a[4][0] = 1
-a[5][4] = 1
-
-print a
-tc = transitiveClosure(a)
-print "\n"
-print tc
